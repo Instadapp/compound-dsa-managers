fcfc956984a3bdc003168c299de248cc92ad6f10
Richa-iitr <richa@cs.iitr.ac.in>
2022-09-05 16:08:45 +0530

    minor fix

--- src/compound-v3-managers.ts
+++ src/compound-v3-managers.ts
@@ -24,7 +24,7 @@ export function handleManagerToggledByAllow(call: AllowCall): void {
   let transactionData = createOrLoadTransaction(transactionID);
   transactionData.txnIndex = call.transaction.index;
   transactionData.from = call.transaction.from;
-  transactionData.to = call.transaction.to;
+  transactionData.to = call.to;
   transactionData.input = call.transaction.input;
   transactionData.blockNumber = call.block.number;
   transactionData.timestamp = call.block.timestamp;
@@ -50,7 +50,7 @@ export function handleManagerToggledByAllow(call: AllowCall): void {
   owner.save();
 }
 
-export function handleMnaagerToggledByPermit(call: AllowBySigCall): void {
+export function handleManagerToggledByPermit(call: AllowBySigCall): void {
   let ownerID = call.inputs.owner.toHexString() + "#" + call.to.toHexString();
   let managerID = call.inputs.manager.toHexString() + "#" + ownerID;
   let transactionID =
@@ -61,7 +61,7 @@ export function handleMnaagerToggledByPermit(call: AllowBySigCall): void {
   let transactionData = createOrLoadTransaction(transactionID);
   transactionData.txnIndex = call.transaction.index;
   transactionData.from = call.transaction.from;
-  transactionData.to = call.transaction.to;
+  transactionData.to = call.to;
   transactionData.input = call.transaction.input;
   transactionData.blockNumber = call.block.number;
   transactionData.timestamp = call.block.timestamp;
